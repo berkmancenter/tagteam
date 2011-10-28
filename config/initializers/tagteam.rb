5bedb46813d24aede5e5ac55751cb1155a35c03d
djcp <dan@collispuro.net>
2011-10-28 11:18:43 -0400

    Timing tweaks.

--- config/initializers/tagteam.rb
+++ config/initializers/tagteam.rb
@@ -2,13 +2,15 @@ ROBOT_USER_AGENT = "tagteam social RSS aggregrator 0.1: http://github.com/berkma
 RSS_GENERATOR = "tagteam social RSS aggregrator"
 
 # If a feed has changed, schedule it to be spidered again within this interval.
-MINIMUM_FEED_SPIDER_INTERVAL = 15.minutes
+MINIMUM_FEED_SPIDER_INTERVAL = 30.minutes
 
-# If a feed has not changed within this interval, back off for SPIDER_DECAY_INTERVAL.
-SPIDER_UPDATE_DECAY = 2.hours
+# If a feed has not changed within this interval, back off for SPIDER_DECAY_INTERVAL. This is set to 6 hours to 
+# ensure we don't entirely forget that a feed is busy over night.
+SPIDER_UPDATE_DECAY = 6.hours
 
 # After SPIDER_UPDATE_DECAY has been reached, extend the next spidering event out by this interval.
 
 SPIDER_DECAY_INTERVAL = 1.hour
 
 MAXIMUM_FEED_SPIDER_INTERVAL = 1.day
+
