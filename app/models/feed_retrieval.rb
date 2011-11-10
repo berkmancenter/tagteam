e19b20a8784802dc9886e84a7841457bc95802dd
djcp <dan@collispuro.com>
2011-11-10 08:22:31 -0500

    changelog view formatting.

--- app/models/feed_retrieval.rb
+++ app/models/feed_retrieval.rb
@@ -5,6 +5,7 @@ class FeedRetrieval < ActiveRecord::Base
   scope :successful, where(['success is true'])
 
   after_save :update_feed_updated_at
+  attr_accessor :changelog_summary_cache
 
   def update_feed_updated_at
     self.feed.updated_at = DateTime.now
@@ -16,26 +17,38 @@ class FeedRetrieval < ActiveRecord::Base
     changes = YAML.load(self.changelog)
   end
 
+  def new_feed_items
+    self.changelog_summary[:new_records]
+  end
+
+  def changed_feed_items
+    self.changelog_summary[:changed_records]
+  end
+
   def changelog_summary
+
+    return self.changelog_summary_cache unless self.changelog_summary_cache.nil?
+
     changelog_yaml = parsed_changelog
     return nil if changelog_yaml.empty?
-
     changes = {
-      :new_records => 0,
-      :changed_fields => []
+      :new_records => [],
+      :changed_records => []
     }
-
     changelog_yaml.keys.each do|ch|
       if changelog_yaml[ch].include?(:new_record)
-        changes[:new_records] = changes[:new_records] + 1
+        changes[:new_records] << ch
       else
         # Something happened to this item
         # FIXME
+        changed_fields = [ch]
         changelog_yaml[ch].keys.each do|change|
-          changes[:changed_fields] << change.to_s.titleize
+         changed_fields << change.to_s.titleize
         end
+        changes[:changed_records] << changed_fields
       end
     end
+    self.changelog_summary_cache = changes
     return changes
   end
 
