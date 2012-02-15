3056c056346376010d43cbd83e9d869e5d075508
djcp <dan@collispuro.com>
2012-02-15 08:05:23 -0500

    Paginate feed retrievals.

--- app/controllers/feed_retrievals_controller.rb
+++ app/controllers/feed_retrievals_controller.rb
@@ -4,7 +4,7 @@ class FeedRetrievalsController < ApplicationController
 
   def index
     breadcrumbs.add @hub_feed, hub_hub_feed_path(:hub_id => @hub, :id => @hub_feed)
-    @feed_retrievals = @hub_feed.feed.feed_retrievals
+    @feed_retrievals = @hub_feed.feed.feed_retrievals.paginate(:page => params[:page], :per_page => params[:per_page])
     render :layout => ! request.xhr?
   end
 
@@ -13,8 +13,8 @@ class FeedRetrievalsController < ApplicationController
     breadcrumbs.add @hub_feed, hub_hub_feed_path(@hub, @hub_feed)
     @feed_retrieval = FeedRetrieval.find(params[:id])
 
-    @new_items = FeedItem.find(:all, :include => [:tags, :taggings, :feeds, :hub_feeds], :conditions => {:id => @feed_retrieval.new_feed_items}, :order => 'created_at desc')
-    @changed_items = FeedItem.find(:all, :include => [:tags, :taggings, :feeds, :hub_feeds], :conditions => {:id => @feed_retrieval.changed_feed_items}, :order => 'created_at desc')
+    @new_items = FeedItem.paginate(:include => [:tags, :taggings, :feeds, :hub_feeds], :conditions => {:id => @feed_retrieval.new_feed_items}, :order => 'created_at desc',:page => params[:page], :per_page => params[:per_page])
+    @changed_items = FeedItem.paginate(:include => [:tags, :taggings, :feeds, :hub_feeds], :conditions => {:id => @feed_retrieval.changed_feed_items}, :order => 'created_at desc',:page => params[:page], :per_page => params[:per_page])
   end
 
   private
