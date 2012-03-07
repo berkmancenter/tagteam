d4d182bf7c82942bf7a8b503855265d652c558b6
djcp <dan@collispuro.com>
2012-03-06 22:21:42 -0500

    Track redirect path after auth failure and use a custom layout for authentication events spawned from some URLs.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :init_breadcrumbs
 
+  layout :layout_by_resource
+
   def get_per_page
     params[:per_page] || cookies[:per_page] || DEFAULT_TAGTEAM_PER_PAGE
+  end
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :user && action_name == 'new' && session[:user_return_to].match(/bookmarklet/)
+      "bookmarklet"
+    else
+      "application"
+    end
+  end
+
+  rescue_from Acl9::AccessDenied do |exception|
+    flash[:notice] = 'Please log in'
+    session[:user_return_to] = request.original_url
+    redirect_to new_user_session_path
   end
 
   private 
