4227c8581afeae93e9094add5e3771e355660b7d
djcp <dan@collispuro.net>
2012-03-16 16:55:36 -0400

    Fixes for web-dispatched cron tasks.

--- app/controllers/scheduled_tasks_controller.rb
+++ app/controllers/scheduled_tasks_controller.rb
@@ -7,7 +7,7 @@ class ScheduledTasksController < ApplicationController
     UpdateFeeds.perform
     render :text => "Updated feeds\n", :layout => false
   rescue Exception => e
-    render :string => e.inspect
+    render :string => e.inspect, :status => :internal_server_error
   end
 
   def expire_cache
@@ -15,7 +15,7 @@ class ScheduledTasksController < ApplicationController
     ExpireFileCache.perform
     render :text => "Expired files from the cache\n", :layout => false
   rescue Exception => e
-    render :string => e.inspect
+    render :string => e.inspect, :status => :internal_server_error
   end
 
   private
@@ -23,7 +23,7 @@ class ScheduledTasksController < ApplicationController
   def check_shared_key
     if params[:SHARED_KEY_FOR_TASKS].nil? || params[:SHARED_KEY_FOR_TASKS].gsub(/['"]/,'') != SHARED_KEY_FOR_TASKS
       flash[:notice] = 'Sorry, dear chap. You need to give me the shared key if you want to run scheduled tasks via a web request.'
-      render :text => "Sorry, the SHARED_KEY_FOR_TASKS didn't match.\n", :layout => false and return
+      render :status => :not_acceptable,  :text => "Sorry, the SHARED_KEY_FOR_TASKS didn't match.\n", :layout => false and return
     end
   end
 
