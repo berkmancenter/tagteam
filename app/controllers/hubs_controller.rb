257a49b3bce364e39e2ce8740942c6da995bf237
Justin Clark <jclark@cyber.law.harvard.edu>
2012-12-20 17:47:55 -0500

    made good progress on moving to Solr 4 and eDisMax

--- app/controllers/hubs_controller.rb
+++ app/controllers/hubs_controller.rb
@@ -440,71 +440,21 @@ class HubsController < ApplicationController
     @hub = Hub.find(params[:id])
     breadcrumbs.add @hub, hub_path(@hub)
 
-    if params[:junction_type].blank?
-      params[:junction_type] = 'and'
-    end
-    # no code injection here.
-    junction_type_map = {'and' => 'all_of', 'or' => 'any_of'}
+    hub_id = @hub.id
 
-    unless params[:include_tag_ids].blank?
-      include_tags = params[:include_tag_ids]
-    end
-
-    unless params[:exclude_tag_ids].blank?
-      exclude_tags = params[:exclude_tag_ids]
-    end
-
-    unless params[:q].blank?
-      keywords = params[:q]
-    end
-
-    unless params[:hub_feed_ids].blank?
-      hub_feed_ids = params[:hub_feed_ids] 
-    end
-
-    if ! include_tags.blank? || ! exclude_tags.blank? || ! keywords.blank? || ! hub_feed_ids.blank?
-      @search = FeedItem.search
-      hub_id = @hub.id
-
-      hub_context = @hub.tagging_key
-
-      @search.build do
-        self.send((junction_type_map[params[:junction_type]].blank?) ? 'any_of' : junction_type_map[params[:junction_type]]) do
-          unless hub_feed_ids.blank?
-            with :hub_feed_ids, hub_feed_ids
-          end
-          unless include_tags.blank?
-            with :tag_contexts, include_tags.collect{|it| %Q|#{hub_context}-#{it}|}
-          end
-          unless exclude_tags.blank?
-            without :tag_contexts, exclude_tags.collect{|it| %Q|#{hub_context}-#{it}|}
-          end
-          unless params[:q].blank?
-            text_fields do
-              any_of do
-                with :title, params[:q]
-                with :description, params[:q]
-                with :content, params[:q]
-                with :url, params[:q]
-                with :guid, params[:q]
-                with :authors, params[:q]
-                with :contributors, params[:q]
-                with :rights, params[:q]
-                with :tag_list, params[:q]
-              end
-            end
-          end
-        end
+    @search = FeedItem.search do
+      fulltext params[:q]
+      with :hub_ids, hub_id
+      paginate :page => params[:page], :per_page => get_per_page
+      order_by(:date_published, :desc)
+      adjust_solr_params do |params|
+        params[:q].gsub! '#', 'tag_list_sm:'
       end
-
-      @search.build do
-        with :hub_ids, hub_id
-        paginate :page => params[:page], :per_page => get_per_page
-        order_by(:date_published, :desc)
-      end
-
-      @search.execute!
     end
+
+    @search.execute!
+    params[:q].gsub! 'tag_list_sm:', '#'
+
     respond_to do|format|
       format.html{ render :layout => ! request.xhr? }
       format.json{ render_for_api :default, :json => (@search.blank?) ? [] : @search.results }
