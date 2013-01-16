df162bf64bb54f2c2feb11e58bc1814a1e3871d4
Justin Clark <jclark@cyber.law.harvard.edu>
2013-01-16 13:51:20 -0500

    closes #3357 and disables caching in dev

--- lib/will_paginate/view_helpers/custom_link_renderer.rb
+++ lib/will_paginate/view_helpers/custom_link_renderer.rb
@@ -4,15 +4,16 @@ module WillPaginate::ViewHelpers
     private
 
     def html_container(html)
+        puts @collection.per_page
       %Q|<div class="page_browsing_controls">#{tag(:div, html, container_attributes)}
       <span class="per_page_selector_container">
     Per Page: <select name="per_page" class="per_page_selector">
-    <option value="10">10</option>
-    <option value="25">25</option>
-    <option value="50">50</option>
-    <option value="100">100</option>
-    <option value="250">250</option>
-    <option value="500">500</option>
+    <option value="10" #{'selected="selected"' if @collection.per_page == 10}>10</option>
+    <option value="25" #{'selected="selected"' if @collection.per_page == 25}>25</option>
+    <option value="50" #{'selected="selected"' if @collection.per_page == 50}>50</option>
+    <option value="100" #{'selected="selected"' if @collection.per_page == 100}>100</option>
+    <option value="250" #{'selected="selected"' if @collection.per_page == 250}>250</option>
+    <option value="500" #{'selected="selected"' if @collection.per_page == 500}>500</option>
     </select>
     </span>
       </div>|
