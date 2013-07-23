6568132dc6f61dba53d40fbe6f337ad04e12a880
Justin Clark <jclark@cyber.law.harvard.edu>
2013-07-23 11:09:38 -0400

    change to devise 2.0 migration

--- db/migrate/20110714150903_devise_create_users.rb
+++ db/migrate/20110714150903_devise_create_users.rb
@@ -5,15 +5,34 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :last_name, :limit => 100
       t.string :url, :limit => 250
 
-      t.database_authenticatable :null => false
-      t.recoverable
-      t.rememberable
-      t.trackable
+      ### Devise
+      ## Database authenticatable
+      t.string :email,              :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
 
-      # t.encryptable
-      t.confirmable
-      t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
-      # t.token_authenticatable
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Trackable
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+
+      ## Lockable
+      t.integer  :failed_attempts, :default => 0
+      t.string   :unlock_token
+      t.datetime :locked_at
 
       t.timestamps
     end
@@ -22,6 +41,5 @@ class DeviseCreateUsers < ActiveRecord::Migration
     add_index :users, :reset_password_token, :unique => true
     add_index :users, :confirmation_token,   :unique => true
     add_index :users, :unlock_token,         :unique => true
-    # add_index :users, :authentication_token, :unique => true
   end
 end
