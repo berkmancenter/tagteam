381fa8d83be229bf15fd128b82db0015b0fd12fb
jdcc <jclark@cyber.law.harvard.edu>
2015-05-01 10:33:26 -0400

    show recalc progress

--- app/workers/recalc_all_items.rb
+++ app/workers/recalc_all_items.rb
@@ -8,11 +8,16 @@ class RecalcAllItems
 
   def perform(hub_id)
     hub = Hub.find(hub_id)
+    bar = ProgressBar.new(
+      hub.hub_feeds.map{|hf| hf.feed_items.count}.sum,
+      :bar, :percentage, :counter, :elapsed, :rate, :eta
+    )
 
-    HubFeed.all(:conditions => {:hub_id => hub_id}).each do |hf|
-      hf.feed_items.each do|fi|
+    hub.hub_feeds.each do |hf|
+      hf.feed_items.find_each(batch_size: 200) do |fi|
         fi.render_filtered_tags_for_hub(hub)
         fi.save
+        bar.increment!
       end
     end
 
