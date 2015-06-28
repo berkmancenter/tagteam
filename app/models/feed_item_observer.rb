4abac0161dd4ee1c8d203e7ff6ef5040f11200a0
jdcc <justind.c.clark@gmail.com>
2015-06-28 10:48:11 -0400

    do not do stuff if feed item removed

--- app/models/feed_item_observer.rb
+++ app/models/feed_item_observer.rb
@@ -4,6 +4,7 @@ class FeedItemObserver < ActiveRecord::Observer
   # create. Actually, after commit:
   # https://github.com/mperham/sidekiq/wiki/Problems-and-Troubleshooting#cannot-find-modelname-with-id12345
   def after_commit(item)
+    return unless item.persisted? # We don't need to run if it's been destroyed
     item.copy_global_tags_to_hubs unless item.skip_global_tag_copy
     Hub.apply_all_tag_filters_to_item_async(item)
   end
