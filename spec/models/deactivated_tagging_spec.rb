5f28edf45034b742e27730f94f6c78cda8d82dca
Patrick Lewis <patrick@endpoint.com>
2017-02-14 16:04:10 -0500

    Fix failing DeactivatedTagging tests

--- spec/models/deactivated_tagging_spec.rb
+++ spec/models/deactivated_tagging_spec.rb
@@ -3,36 +3,24 @@ require 'rails_helper'
 
 RSpec.describe DeactivatedTagging, type: :model do
   describe '#reactivate' do
-    it 'copies itself into the taggings table' do
-      tagging = create(:tagging)
-      create(:delete_tag_filter).deactivate_tagging(tagging)
-      expect(ActsAsTaggableOn::Tagging.count).to eq(0)
+    let(:delete_tag_filter) { create(:delete_tag_filter) }
+    let(:tagging) { create(:tagging) }
+    let(:reactivated_tagging) { @deactivated_tagging.reactivate }
 
-      d_tagging = described_class.first
-      d_tagging.reactivate
-      expect(ActsAsTaggableOn::Tagging.first).to eq(tagging)
+    before do
+      @deactivated_tagging = delete_tag_filter.deactivate_tagging(tagging)
+    end
+
+    it 'copies itself into the taggings table' do
+      expect { reactivated_tagging }.to change { ActsAsTaggableOn::Tagging.count }.by(1)
     end
 
     it 'removes itself from the deactivated taggings table' do
-      tagging = create(:tagging)
-      create(:delete_tag_filter).deactivate_tagging(tagging)
-      expect(ActsAsTaggableOn::Tagging.count).to eq(0)
-
-      d_tagging = described_class.first
-      d_tagging.reactivate
-
-      expect(described_class.count).to eq(0)
+      expect { reactivated_tagging }.to change { described_class.count }.by(-1)
     end
 
     it 'returns the reactivated copy of itself' do
-      tagging = create(:tagging)
-      create(:delete_tag_filter).deactivate_tagging(tagging)
-      expect(ActsAsTaggableOn::Tagging.count).to eq(0)
-
-      d_tagging = described_class.first
-      new_tagging = d_tagging.reactivate
-
-      expect(new_tagging).to eq(tagging)
+      expect(reactivated_tagging).to eq(tagging)
     end
   end
 end
