e76d5e2c4fa5261a998ca82e9c37c439f138507e
Patrick Lewis <patrick@endpoint.com>
2017-03-09 16:52:40 -0500

    Add hub_feed_updated helper

---
+++ app/helpers/hub_feeds_helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+# Helper methods for hub feeds
+module HubFeedsHelper
+  def hub_feed_updated(hub_feed)
+    updated_at =
+      if hub_feed.latest_feed_items.any?
+        hub_feed.latest_feed_items.first.updated_at
+      else
+        hub_feed.feed.updated_at
+      end
+
+    updated_at.to_s(:long)
+  end
+end
