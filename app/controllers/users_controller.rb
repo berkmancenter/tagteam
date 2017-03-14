4ca5f203bf10ba149712736515b6a87a49503dc6
Peter Hankiewicz <peterh@endpoint.com>
2017-03-14 16:05:12 -0400

    Created a new view for showing user items and user items with specific tags applied
    Conflicts:
    
    	app/controllers/tag_filters_controller.rb
    	app/views/users/tags.html.haml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,36 @@
 # frozen_string_literal: true
 class UsersController < ApplicationController
-  before_action :authenticate_user!, except: [:tags, :user_tags]
+  before_action :authenticate_user!, except: [:tags, :user_tags, :tags_json,
+                                              :tags_rss, :tags_atom]
   before_action :load_user, only: [:roles_on]
+  before_action :load_hub, only: [:tags, :tags_json, :tags_rss, :tags_atom]
+  before_action :load_feed_items, only: [:tags, :tags_json, :tags_rss,
+                                         :tags_atom]
+  before_action :set_home_url, only: [:tags, :tags_json, :tags_rss,
+                                      :tags_atom]
 
   after_action :verify_authorized
 
   def tags
-    @hub = Hub.find(params[:hub_id])
     breadcrumbs.add @hub, hub_path(@hub)
-    @user = User.find_by(username: params[:username])
-    authorize @user
-    @show_auto_discovery_params = hub_user_tags_rss_url(@hub, @user)
-    @feed_items = ActsAsTaggableOn::Tagging.where(context: "hub_#{@hub.id}",
-                                                  taggable_type: 'FeedItem',
-                                                  tagger_id: @user,
-                                                  tagger_type: 'User')
-                                           .paginate(page: params[:page], per_page: get_per_page)
-    render layout: !request.xhr?
+    breadcrumbs.add @user.username, hub_user_tags_path(@hub, @user.username)
+    if @tag
+      breadcrumbs.add @tag.name, hub_user_tags_name_path(
+        @hub, @user.username, @tag.name
+      )
+    end
+
+    template = params[:view] == 'grid' ? 'tags_grid' : 'tags'
+    render template, layout: request.xhr? ? false : 'tabs'
   end
+
+  def tags_json
+    render_for_api :default, json: @feed_items
+  end
+
+  def tags_rss; end
+
+  def tags_atom; end
 
   def user_tags
     @hub = Hub.find(params[:hub_id])
@@ -108,5 +121,45 @@ class UsersController < ApplicationController
               current_user
             end
     authorize @user
+  end
+
+  def load_hub
+    @hub = Hub.find(params[:hub_id])
+  end
+
+  def set_home_url
+    @home_url = if @tag
+                  hub_user_tags_name_path(
+                    @hub, @user.username, @tag.name
+                  )
+                else
+                  hub_user_tags_path(@hub, @user.username)
+                end
+  end
+
+  def load_feed_items
+    authorize User
+
+    @user = User.find_by(username: params[:username])
+
+    if params[:tagname]
+      @tag = ActsAsTaggableOn::Tag.where(name: params[:tagname]).first
+
+      taggings = ActsAsTaggableOn::Tagging.select('DISTINCT ON ("context") *')
+                                          .where(context: "hub_#{@hub.id}",
+                                                 taggable_type: 'FeedItem',
+                                                 tag_id: @tag,
+                                                 tagger_id: @user,
+                                                 tagger_type: 'User')
+    else
+      taggings = ActsAsTaggableOn::Tagging.select('DISTINCT ON ("context") *')
+                                          .where(context: "hub_#{@hub.id}",
+                                                 taggable_type: 'FeedItem',
+                                                 tagger_id: @user,
+                                                 tagger_type: 'User')
+    end
+
+    @feed_items = FeedItem.where(id: taggings.pluck(:taggable_id))
+                          .paginate(page: params[:page], per_page: get_per_page)
   end
 end
---
+++ app/views/users/tags_rss.builder
@@ -0,0 +1,40 @@
+xml.instruct! :xml, :version => "1.0" 
+xml.rss(
+  :version => '2.0',
+  'xmlns:content' => 'http://purl.org/rss/1.0/modules/content/',
+  'xmlns:dc' => 'http://purl.org/dc/elements/1.1/'
+  ) do
+  xml.channel do
+    xml.title "Items tagged by #{@user.username} in #{@hub.title}"
+    xml.description "Items tagged by #{@user.username} in #{@hub.title}"
+    xml.link @home_url
+    xml.generator Tagteam::Application.config.rss_generator
+
+    @feed_items.each do |item|
+      xml.item do
+        xml.title item.title
+        unless item.description.blank?
+          xml.description item.description
+        end
+        unless item.content.blank?
+          xml.tag!('content:encoded', item.content)
+        end
+        unless item.date_published.blank?
+          xml.pubDate item.date_published.to_s(:rfc822)
+        end
+        xml.link item.url
+        xml.guid item.guid
+        xml.author item.authors
+        item.all_tags_on(@hub.tagging_key).each do|tag|
+          xml.category (@hub.tag_prefix.blank?) ? tag.name : tag.name_prefixed_with(@hub.tag_prefix)
+        end
+        unless item.rights.blank?
+          xml.tag!('dc:rights', item.rights)
+        end
+        unless item.contributors.blank?
+          xml.tag!('dc:contributor', item.contributors)
+        end
+      end
+    end
+  end
+end
