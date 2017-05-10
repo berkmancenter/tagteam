477944f0cb13a276612215507820704d65fd08b7
Patrick Lewis <patrick@endpoint.com>
2017-05-10 17:03:53 -0400

    Add validation tests for RepublishedFeeds

--- spec/models/republished_feed_spec.rb
+++ spec/models/republished_feed_spec.rb
@@ -6,5 +6,7 @@ RSpec.describe RepublishedFeed, type: :model do
     subject { create(:republished_feed) }
 
     it { is_expected.to validate_uniqueness_of(:url_key) }
+    it { is_expected.to allow_value('foo').for(:url_key) }
+    it { is_expected.not_to allow_value('@_@').for(:url_key) }
   end
 end
