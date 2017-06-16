5487f394685c0e8de8eb40c726190a57a6b1369f
Patrick Lewis <patrick@endpoint.com>
2017-06-16 10:00:04 -0400

    Update #12481 Fix date displayed for bookmark collections to match published date of most recent feed item

--- app/helpers/hub_feeds_helper.rb
+++ app/helpers/hub_feeds_helper.rb
@@ -3,8 +3,8 @@
 module HubFeedsHelper
   def hub_feed_updated(hub_feed)
     updated_at =
-      if hub_feed.most_recent_tagging.present?
-        hub_feed.most_recent_tagging.created_at
+      if hub_feed.feed_items.any?
+        hub_feed.feed_items.first.date_published
       else
         hub_feed.feed.updated_at
       end
