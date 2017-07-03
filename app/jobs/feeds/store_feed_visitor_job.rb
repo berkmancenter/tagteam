5d11fc26f19c36992da70868d6c212f809ded809
Patrick Lewis <patrick@endpoint.com>
2017-07-03 10:43:47 -0400

    Refactor StoreFeedVisitorJob

--- app/jobs/feeds/store_feed_visitor_job.rb
+++ app/jobs/feeds/store_feed_visitor_job.rb
@@ -5,21 +5,13 @@ module Feeds
   class StoreFeedVisitorJob < ApplicationJob
     queue_as :subscribers
 
-    def perform(route, feed_type, user_ip, user_agent)
+    def perform(route, feed_type, ip, user_agent)
       browser = Browser.new(user_agent, {})
 
       return unless %w[rss atom json].include? feed_type
       return if browser.bot?
 
-      feed_visitor = FeedVisitor.new
-
-      feed_visitor.attributes = {
-        route: route,
-        ip: user_ip,
-        user_agent: user_agent
-      }
-
-      feed_visitor.save!
+      FeedVisitor.create!(route: route, ip: ip, user_agent: user_agent)
     end
   end
 end
