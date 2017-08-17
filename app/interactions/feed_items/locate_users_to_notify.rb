b52dc4186938cfea723704d0342a658498ed7bc7
Patrick Lewis <patrick@endpoint.com>
2017-08-17 11:13:41 -0400

    Update #14338 Fix notifications for feed items with no users to notify

--- app/interactions/feed_items/locate_users_to_notify.rb
+++ app/interactions/feed_items/locate_users_to_notify.rb
@@ -1,29 +1,31 @@
 # frozen_string_literal: true
 
 module FeedItems
-  # Return a collection of users to be notified of tagging changes to a feed item in a given hub
+  # Return a collection of users to be notified of changes to a feed item in a given hub
   class LocateUsersToNotify < ActiveInteraction::Base
     object :feed_item
     object :hub
     object :current_user, class: User
 
     def execute
-      users_to_notify = []
+      users_to_notify = tag_filter_users | tagging_users
 
-      users_to_notify.concat(
-        compose(FeedItems::LocateTagFilterUsers, feed_item: feed_item)
-      )
+      return users_to_notify if users_to_notify.empty?
 
-      users_to_notify.concat(
-        compose(FeedItems::LocateTaggingUsersByHub, feed_item: feed_item, hub: hub)
-      )
-
-      users_to_notify.uniq!.delete(current_user)
+      users_to_notify.delete(current_user)
 
       # remove users who don't have modification notifications enabled
-      users_to_notify.select do |user|
-        user.hub_user_notifications.find_by(hub: hub).try(:notify_about_modifications?)
-      end
+      users_to_notify.select { |user| user.notifications_for_hub?(hub) }
+    end
+
+    private
+
+    def tag_filter_users
+      compose(FeedItems::LocateTagFilterUsers, feed_item: feed_item)
+    end
+
+    def tagging_users
+      compose(FeedItems::LocateTaggingUsersByHub, feed_item: feed_item, hub: hub)
     end
   end
 end
