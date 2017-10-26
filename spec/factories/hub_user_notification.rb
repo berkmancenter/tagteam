4e45ff5c8b0fcebd4a374ea1ba2b649517992939
Patrick Lewis <patrick@endpoint.com>
2017-10-26 14:14:59 +0000

    Remove duplicate hub_user_notification factory

--- spec/factories/hub_user_notification.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-FactoryGirl.define do
-  factory :hub_user_notification, class: HubUserNotification do
-    hub
-    user
-    notify_about_modifications { true }
-  end
-end
