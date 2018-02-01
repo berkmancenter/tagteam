f2b77aa33fb75e32a614388c681fd5cee79e873a
Gaurav Soni <gaurav@endpoint.com>
2018-02-01 18:38:11 +0530

    Fixed, Tried to load unspecified class: Documentation Error

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -13,7 +15,8 @@ require 'digest/md5'
 
 # get the Documentation class in here.
 Documentation.destroy_all
-docs = YAML.safe_load(File.read("#{Rails.root}/db/documentation.yml"))
+docs = YAML.safe_load(File.read(Rails.root.join('db', 'documentation.yml')),
+                      [Documentation, Time])
 docs.each do |doc_attr|
   d = Documentation.find_or_initialize_by(match_key: doc_attr.attributes[:match_key])
   d.attributes = doc_attr.attributes
