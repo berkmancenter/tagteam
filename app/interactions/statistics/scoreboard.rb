15a0aaa93b92494ffe0a546a34cb7f6b5eee25d4
Patrick Lewis <patrick@endpoint.com>
2018-03-21 19:09:10 +0000

    Lint Statistics::Scoreboard interaction

--- app/interactions/statistics/scoreboard.rb
+++ app/interactions/statistics/scoreboard.rb
@@ -7,7 +7,7 @@ module Statistics
     string :criteria, default: 'Past Day'
 
     def execute
-      users = @hub.users_with_roles.map{|u| {id: u.id, first_name: u.first_name , last_name: u.last_name} }
+      users = @hub.users_with_roles.map { |u| { id: u.id, first_name: u.first_name, last_name: u.last_name } }
 
       taggings = ActsAsTaggableOn::Tagging.where(created_at: set_criteria)
 
@@ -32,15 +32,15 @@ module Statistics
 
     def set_criteria
       case @criteria
-        when 'Past Day'
-          1.day.ago.beginning_of_day..1.day.ago.end_of_day
-        when 'Week'
-          1.week.ago.beginning_of_day..(1.week.ago + 7.day).end_of_day
-        when 'Month'
-          1.month.ago.beginning_of_day..(1.week.ago + 30.day).end_of_day
-        when 'Year'
-          1.year.ago.beginning_of_day..(1.year.ago + 365.day).end_of_day         
-      end 
+      when 'Past Day'
+        1.day.ago.beginning_of_day..1.day.ago.end_of_day
+      when 'Week'
+        1.week.ago.beginning_of_day..(1.week.ago + 7.days).end_of_day
+      when 'Month'
+        1.month.ago.beginning_of_day..(1.week.ago + 30.days).end_of_day
+      when 'Year'
+        1.year.ago.beginning_of_day..(1.year.ago + 365.days).end_of_day
+      end
     end
   end
 end
