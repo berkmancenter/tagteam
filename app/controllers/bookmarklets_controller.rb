2b497e82c870406706056b9e8ac1dda43b6c7fb8
Patrick Lewis <patrick@endpoint.com>
2018-04-03 14:46:33 +0000

    Lint BookmarkletsController

--- app/controllers/bookmarklets_controller.rb
+++ app/controllers/bookmarklets_controller.rb
@@ -90,7 +90,7 @@ class BookmarkletsController < ApplicationController
 
         new_tags -= @hub.deprecated_tag_names
 
-        new_tags.map!{|tag| tag.gsub(',', '.')}
+        new_tags.map! { |tag| tag.tr(',', '.') }
 
         new_tags.map do |tag|
           ActsAsTaggableOn::Tag.normalize_name(tag)
