504c56cbf99b935422515bc663accd6a2f4b9a27
Steph Skardal <steph@endpoint.com>
2018-08-22 17:46:34 +0000

    Added in migration from ActsAsTaggable update.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180816161305) do
+ActiveRecord::Schema.define(version: 20180822171418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -311,6 +311,7 @@ ActiveRecord::Schema.define(version: 20180816161305) do
 
   create_table "tags", id: :serial, force: :cascade do |t|
     t.string "name", limit: 255
+    t.integer "taggings_count", default: 0
     t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
