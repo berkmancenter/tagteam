ecc4499631c9ccb877a74a342f3498b391545101
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2019-06-03 15:35:11 +0200

    Mount Rails files as Docker volume

--- docker/start_dev.sh
+++ docker/start_dev.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cd /app
+# capybara-webkit needs it
+export PATH="/usr/lib/qt5/bin:$PATH"
+bundle install
 touch log/sidekiq.log
 cp -n docker/database.yml config/database.yml
 cp -n config/tagteam.yml.example config/tagteam.yml
